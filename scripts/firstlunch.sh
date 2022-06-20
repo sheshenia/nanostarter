62f82edc74b2df4962a3b623982995fcc44c1bb9
Pavlo Sheshenia <sheshenia@outlook.com>
2022-06-20 16:12:31 +0300

    Clean up

--- scripts/firstlunch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-mkdir ../scep && cd ../scep
-
-curl -RLO https://github.com/micromdm/scep/releases/download/v2.1.0/scepserver-linux-amd64-v2.1.0.zip
-
-unzip scepserver-linux-amd64-v2.1.0.zip
-
-./scepserver-linux-amd64 ca -init
